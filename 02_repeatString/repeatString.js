61239e9363de6ad5b14cb752c58cd74e07a7ea5a
DevSock <mikeashley227@yahoo.com>
2022-05-17 19:50:35 -0400

    Add repetition logic to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, reps) {
+  // If reps is negative, return "ERROR"
+  // If reps === 0, return ""
+  // If reps > 0, return string repeated reps times
+  return reps < 0 ? "ERROR" : string.repeat(reps);
 };
 
 // Do not edit below this line
