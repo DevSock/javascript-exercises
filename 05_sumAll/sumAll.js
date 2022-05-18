bb9c53327865f8423dd9211757fd65d58f2e772d
DevSock <mikeashley227@yahoo.com>
2022-05-17 20:29:56 -0400

    Implement gaussian summing & remove test skips

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  return ((num2 - num1 + 1) * (num1 + num2)) / 2;
 };
 
 // Do not edit below this line
