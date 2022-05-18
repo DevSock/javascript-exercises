2aec661ce68f813e186ce81addd841b7bbd06e18
DevSock <mikeashley227@yahoo.com>
2022-05-17 20:00:44 -0400

    Solve problem & remove test skips

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let strLength = string.length - 1;
+  let result = "";
+  while (strLength >= 0) {
+    result += string[strLength];
+    strLength--;
+  }
+  return result;
 };
 
 // Do not edit below this line
